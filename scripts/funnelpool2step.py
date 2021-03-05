df6aac7e3eeea9a407d88f905591dce74c43e5bd
modichirag <cmodi@flatironinstitute.org>
2021-03-06 01:20:26 +0530

    2 step hmc

---
+++ scripts/funnelpool2step.py
@@ -0,0 +1,171 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import sys, os
+import  multiprocessing as mp
+import multiprocessing as mp
+
+from pyhmc import PyHMC_2step
+import diagnostics as dg
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--ndim',  default=2, type=int, help='Dimensions')
+parser.add_argument('--nsamples',  default=10000, type=int, help='Number of ssmples')
+parser.add_argument('--burnin',  default=1000, type=int, help='Number of burning samples')
+parser.add_argument('--lpath',  default=5, type=float, help='Nleapfrog*step_size')
+parser.add_argument('--step_size', default=0.1, type=float,
+                    help='sum the integers (default: find the max)')
+parser.add_argument('--two_factor', default=2, type=float,
+                    help='sum the integers (default: find the max)')
+parser.add_argument('--nchains',  default=10, type=int, help='Number of chains')
+parser.add_argument('--nparallel',  default=8, type=int, help='Number of parallel iterations for map')
+
+parser.add_argument('--suffix', default='', type=str,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+ndim = args.ndim
+step_size = args.step_size
+two_factor = args.two_factor
+nsamples = args.nsamples
+Lpath = args.lpath
+suffix = args.suffix
+burnin = args.burnin
+nchains = args.nchains
+nparallel = args.nparallel
+Nleapfrog = int(Lpath / step_size)
+
+
+
+np.random.seed(100)
+def mute():
+    sys.stdout = open(os.devnull, 'w')    
+
+
+if ndim < 2:
+    print('atleast 2 dimnesions')
+    import sys
+    sys.exit()
+#
+
+print("\nFor %d dimensions with step size %0.3f and %d steps\n"%(ndim, step_size, Nleapfrog))
+##
+fpath = './outputs/Ndim%02d//'%ndim
+try: os.makedirs(fpath)
+except Exception as e: print(e)
+fpath = fpath + 'step%03d_nleap%02d_fac%02d/'%(step_size*100, Nleapfrog, two_factor)
+try: os.makedirs(fpath)
+except Exception as e: print(e)
+print("output in : ", fpath)
+
+
+#######
+
+def get_logprob():
+    def save_model(obj, filename):
+        """Save compiled models for reuse."""
+        import pickle
+        with open(filename, 'wb') as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def load_model(filename):
+        """Reload compiled models for reuse."""
+        import pickle
+        return pickle.load(open(filename, 'rb'))
+
+
+    model_code_funnel = """
+    parameters {
+    real v; 
+    vector[%s] theta;
+    }
+    
+    
+    model {
+    v ~ normal(0, 3);
+    theta ~ normal(0, exp(v/2));
+    }
+    """%(ndim-1)
+
+
+    fname = './models/funnel_%sdim.pkl'%ndim
+
+    start = time.time()
+    try:
+        sm_funnel = load_model(fname)
+        print("Model loaded from %s"%fname, sm_funnel)
+    except Exception as e:
+        print(e)
+        sm_funnel = pystan.StanModel(model_code=model_code_funnel)
+        save_model(sm_funnel, fname)
+        print("model saved in %s"%fname)
+
+    print("Time to make model : ", time.time()-start)
+
+
+
+
+    start = time.time()
+    samples = sm_funnel.sampling(iter=1, chains=1, algorithm="HMC", seed=100, n_jobs=1, verbose=False,
+                         control={"stepsize":step_size, 
+                                    "adapt_t0":False,
+                                    "adapt_delta":False,
+                                    "adapt_kappa":False,
+                                    "metric":"unit_e",
+                                  "int_time":Lpath
+                                    })
+    end = time.time()
+
+    ##LOG PROB
+    log_prob = samples.log_prob
+    grad_log_prob = samples.grad_log_prob
+
+    return log_prob, grad_log_prob
+        
+
+#############################
+
+log_prob, grad_log_prob = get_logprob()    
+hmc = PyHMC_2step(log_prob, grad_log_prob)
+def step(x):
+    return  hmc.hmc_step(x, Nleapfrog, step_size, two_factor)
+
+
+def do_hmc(pool):
+    
+    samples = []
+    accepts = []
+    probs = []
+    start = time.time()
+    initstate = np.random.uniform(-1., 1., size=nchains*ndim).reshape([nchains, ndim])
+    q = initstate
+
+    for i in range(nsamples + burnin):
+        out = pool.map(step, q)
+        q = [i[0] for i in out] 
+        acc = [i[2] for i in out] 
+        prob = [i[3] for i in out] 
+        samples.append(q)
+        accepts.append(acc)
+        probs.append(prob)
+        
+    end = time.time()
+    print(end - start)
+    mysamples = np.array(samples)[burnin:]
+    accepted = np.array(accepts)[burnin:]
+    probs = np.array(probs)[burnin:]
+    print(mysamples.shape)
+    np.save(fpath + '/samples', mysamples)
+    np.save(fpath + '/accepted', accepted)
+    np.save(fpath + '/probs', probs)
+#
+    dg.plot_hist(mysamples, fpath)
+    dg.plot_trace(mysamples, fpath)
+    dg.plot_scatter(mysamples, fpath)
+    dg.plot_autorcc(mysamples, fpath)
+#    
+if __name__=="__main__":
+    pool = mp.Pool(nparallel, initializer=mute)
+    do_hmc(pool)
--- scripts/pyhmc.py
+++ scripts/pyhmc.py
@@ -55,3 +55,90 @@ class PyHMC():
         q, p, accepted, prob = self.metropolis([q, p], [q1, p1])
         return q, p, accepted, prob
 
+
+
+
+class PyHMC_2step():
+    
+    def __init__(self, log_prob, grad_log_prob, KE=None, KE_g=None):
+
+        self.log_prob, self.grad_log_prob = log_prob, grad_log_prob
+        self.V = lambda x : self.log_prob(x)*-1.
+        self.V_g = lambda x : self.grad_log_prob(x)*-1.
+        
+        if KE is None or KE_g is None:
+            self.KE = self.unit_norm_KE
+            self.KE_g = self.unit_norm_KE_g
+            
+    def unit_norm_KE(self, p):
+        return 0.5 * (p**2).sum()
+
+    def unit_norm_KE_g(self, p):
+        return p
+
+    def H(self, q,p):
+        return self.V(q) + self.KE(p)
+
+
+    def leapfrog(self, q, p, N, step_size):
+        q0, p0 = q, p
+        p = p - 0.5*step_size * self.V_g(q) 
+        for i in range(N-1):
+            q = q + step_size * self.KE_g(p)
+            if np.isnan(q).sum(): return q0, p0
+            p = p - step_size * self.V_g(q) 
+        q = q + step_size * self.KE_g(p)
+        if np.isnan(q).sum(): return q0, p0
+        p = p - 0.5*step_size * self.V_g(q) 
+        return q, p
+
+
+
+
+    def hmc_step(self, q, N, step_size, two_factor):
+        p = np.random.normal(size=q.size).reshape(q.shape)
+        q1, p1 = self.leapfrog(q, p, N, step_size)
+        accepted = False
+        H0 = self.H(q, p)
+        H1 = self.H(q1, p1)
+        prob1 = min(1., np.exp(H0 - H1))
+        if np.isnan(prob1) or (q-q1).sum()==0:
+            prob1 = 0.   ##since prob1 = 1. if q == q1
+            accepted = False
+        elif np.random.uniform(size=1) > prob1:
+            accepted = False
+        else:
+            accepted = True
+        #
+        if accepted:
+            return q1, p1, 1., [H0, H1, H0, H1]
+        else:
+            N2 = int(N*two_factor)
+            s2 = step_size/two_factor
+            print(N2, s2)
+            q2, p2 = self.leapfrog(q, p, N2, s2)
+            H2 = self.H(q2, p2)
+            prob2 = np.exp(H0 - H2)
+            if np.isnan(prob2) or (q-q2).sum()==0: 
+                return q, p, -1., [H0, H1, H2, H1]
+                accepted = False
+            else:
+                q21, p21 = self.leapfrog(q2, -p2, N, step_size)
+                H21 = self.H(q21, p21)
+                prob21 = min(1., np.exp(H2 - H21))
+                
+                if np.isnan(prob1): prob1 = 0.
+                if np.isnan(prob21): prob21 = 0.
+                if prob1 == 1:
+                    import sys
+                    print("prob1 should not be 1")
+                    #sys.exit()
+                prob = min(1., prob2 * (1.-prob21)/(1.-prob1))
+                
+                if np.isnan(prob)  :
+                    return q, p, -1, [H0, H1, H2, H21]
+                elif np.random.uniform(size=1) > prob:
+                    return q, p, 0., [H0, H1, H2, H21]
+                else:
+                    return q2, p2, 2., [H0, H1, H2, H21]
+                
