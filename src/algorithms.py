f411236bdc4136eeabcd828c9c41f4fe7582a592
modichirag <cmodi@flatironinstitute.org>
2022-09-22 11:46:59 -0400

    dual avg stepsize adaptiation

--- src/algorithms.py
+++ src/algorithms.py
@@ -19,7 +19,42 @@ class Sampler():
             if type(self.__dict__[key]) == np.ndarray:
                 self.__dict__[key] = list(self.__dict__[key])
         
+    def save(self, path):
+        pass
 
+
+
+class DualAveragingStepSize():
+    def __init__(self, initial_step_size, target_accept=0.65, gamma=0.05, t0=10.0, kappa=0.75):
+        self.mu = np.log(10 * initial_step_size)  # proposals are biased upwards to stay away from 0.
+        self.target_accept = target_accept
+        self.gamma = gamma
+        self.t = t0
+        self.kappa = kappa
+        self.error_sum = 0
+        self.log_averaged_step = 0
+
+    def update(self, p_accept):
+        # Running tally of absolute error. Can be positive or negative. Want to be 0.
+        self.error_sum += self.target_accept - p_accept
+
+        # This is the next proposed (log) step size. Note it is biased towards mu.
+        log_step = self.mu - self.error_sum / (np.sqrt(self.t) * self.gamma)
+ 
+        # Forgetting rate. As `t` gets bigger, `eta` gets smaller.
+        eta = self.t ** -self.kappa
+
+        # Smoothed average step size
+        self.log_averaged_step = eta * log_step + (1 - eta) * self.log_averaged_step
+
+        # This is a stateful update, so t keeps updating
+        self.t += 1
+
+        # Return both the noisy step size, and the smoothed step size
+        return np.exp(log_step), np.exp(self.log_averaged_step)
+
+
+                
 
 class HMC():
 
@@ -151,22 +186,44 @@ class HMC():
         self.nleap = kwargs['nleap']
 
 
-    def sample(self, q, p=None, callback=None, **kwargs):
+    def adapt_stepsize(self, q, epsadapt):
+        print("Adapting step size for %d iterations"%epsadapt)
+        step_size = self.step_size
+        epsadapt_kernel = DualAveragingStepSize(step_size)
+        for i in range(epsadapt+1):
+            q, p, acc, Hs, count = self.step(q, self.nleap, step_size)
+            prob = np.exp(Hs[0] - Hs[1])
+            if i < epsadapt:
+                if np.isnan(prob): prob = 0.
+                if prob > 1: prob = 1.
+                step_size, avgstepsize = epsadapt_kernel.update(prob)
+            elif i == epsadapt:
+                _, step_size = epsadapt_kernel.update(prob)
+                print("Step size fixed to : ", step_size)
+                self.step_size = step_size
+        return q
+
+    
+    def sample(self, q, p=None, callback=None, skipburn=True, epsadapt=0, **kwargs):
 
         kw = kwargs
         self._parse_kwargs_sample(**kwargs)
         
         state = Sampler()
 
+        if epsadapt:
+            q = self.adapt_stepsize(q, epsadapt)
+            
         for i in range(self.nsamples + self.burnin):
             q, p, acc, Hs, count = self.step(q, self.nleap, self.step_size)
             state.i += 1
-            state.samples.append(q)
             state.accepts.append(acc)
-            state.Hs.append(Hs)
-            state.counts.append(count)
-            if callback is not None: callback(state)
-
+            if skipburn & (i > self.burnin):
+                state.samples.append(q)
+                state.Hs.append(Hs)
+                state.counts.append(count)
+                if callback is not None: callback(state)
+            
         state.to_array()
         return state
 
